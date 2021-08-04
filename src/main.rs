478fdedacdfe8ead06ee2d47b07871a3075386cb
EdvinAlvarado <ealvarado301@gmail.com>
2021-08-03 19:31:43 -0700

    position can now be handled with pos_handler

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+// std 
 use std::convert::TryInto;
 use std::time::{Duration, Instant};
 
+// extern crates
 extern crate rand;
 use rand::{Rng, thread_rng};
 
@@ -9,6 +11,7 @@ use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 
+// project mod
 mod tetris;
 use tetris::Tetromino;
 mod board;
@@ -16,10 +19,7 @@ use board::Board;
 mod interface;
 use interface::IO;
 
-struct Position {
-    x: usize,
-    y: usize,
-}
+struct Position {x: i32, y: i32,}
 
 fn main() {
     // Init randomizer
@@ -34,15 +34,13 @@ fn main() {
     io.canvas.clear();
     io.canvas.present();
     // Init game logic
-    let mut pos = Position{x: board::board_size().0/2, y: 0};
-    let mut piece_type: u32 = rng.gen_range(1..=7);
-    let mut piece = Tetromino::new(piece_type);
+    let mut pos = Position{x: board::board_size().0 as i32 /2, y: 0}; // Assumes a 5x5 piece
+    let mut piece = Tetromino::new(rng.gen_range(1..=7));
     let mut board = Board{..Default::default()};
     let mut timer = Instant::now();
-    let mut is_blocked = board.collision_checker(pos.x, pos.y, &piece);
 
     'main: loop {
-        is_blocked = board.collision_checker(pos.x, pos.y, &piece);
+        let mut is_blocked = board.collision_checker(pos.x, pos.y, &piece);
         'event: for event in event_pump.poll_iter() {
             match event {
                 Event::Quit{..} | Event::KeyDown{keycode: Some(Keycode::Escape), ..}    => {break 'main},
@@ -50,8 +48,9 @@ fn main() {
                 Event::KeyDown{keycode: Some(Keycode::Left), ..} => {if !is_blocked.left {pos.x -= 1;}},
                 Event::KeyDown{keycode: Some(Keycode::Right), ..} => {if !is_blocked.right {pos.x += 1;}},
                 Event::KeyDown{keycode: Some(Keycode::Z), ..} => {if !is_blocked.rotate {piece.rotate();}},
-                _                                                                       => {},
+                _ => {for ev in event_pump.poll_iter() {match ev {_ => {}}}},
             }
+            is_blocked = board.collision_checker(pos.x, pos.y, &piece);
             break 'event;
         }
         if timer.elapsed().as_millis() >= 500 {
@@ -60,14 +59,16 @@ fn main() {
             else {
                 board.write_back_board(pos.x, pos.y, &piece);
                 board.filled_line_cleaner();
-                pos = Position{x: board::board_size().0, y: 0};
-                piece = Tetromino::new(piece_type);
+                pos = Position{x: board::board_size().0 as i32/2, y: 0};
+                piece = Tetromino::new(rng.gen_range(1..=7));
             }
         }
         board.write_board(pos.x, pos.y, &piece);
         io.draw_board(&board);
         io.draw_overlay();
         io.canvas.present();
+        // TODO Debug
+        println!("{}\t{}", pos.x, pos.y);
         std::thread::sleep(Duration::from_millis(50));
     }
     println!("Game Over!! score: {}", board.score);
