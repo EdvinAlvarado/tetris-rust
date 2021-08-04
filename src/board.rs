ac837ba50f93687ac08c4abe7c6afe7e68d05709
EdvinAlvarado <ealvarado301@gmail.com>
2021-08-03 19:30:36 -0700

    included handler to manage i32 inside a array position index

--- src/board.rs
+++ src/board.rs
@@ -1,9 +1,30 @@
 use std::convert::TryInto;
+use std::io::{Error, ErrorKind};
 // mod tetris; // cant use mod if not it thinks it is under board directory just add mod in main
 // file and use the following code line.
 use crate::tetris;
 
-pub const fn board_size() -> (usize, usize) {(28, 56)}
+// Handles adding usize and i32 and it will protect against overflow values.
+pub fn pos_handler(u: usize, i: i32) -> Result<usize,i32> {
+    let sum = u as i32 + i;
+    if sum < 0 {Err(sum)} else {Ok(sum as usize)} // the number of Err does not matter
+}
+
+#[macro_export]
+macro_rules! ph_u {
+    ($u:expr, $i:expr) => {
+        pos_handler($u,$i).unwrap()
+    }
+}
+
+#[macro_export]
+macro_rules! ph_uod {
+    ($u:expr, $i:expr) => {
+        pos_handler($u,$i).unwrap_or_default()
+    }
+}
+
+pub const fn board_size() -> (usize, usize) {(20, 40)}
 
 pub struct Blocked {
     pub left: bool,
@@ -24,24 +45,27 @@ impl Board {
         let line = self.board.get(line_index).expect("line out of range");
         !line.iter().any(|x| x == &0)
     }
-    fn pos_inbound(&self, x: usize, y: usize) -> bool {
-        (0..board_size().0).contains(&x) || (0..board_size().1).contains(&y)
+    fn pos_inbound(&self, x: i32, y: i32) -> Result<(), ErrorKind> {
+        if (0..board_size().0 as i32).contains(&x) || (0..board_size().1 as i32).contains(&y) {
+            return Ok(());
+        }
+        Err(ErrorKind::InvalidInput)
     }
-    pub fn write_board(&mut self, x: usize, y: usize, piece: &tetris::Tetromino) {
+    pub fn write_board(&mut self, x: i32, y: i32, piece: &tetris::Tetromino) {
         self.board = self.back_board;
-        for j in piece.pos_delta[1]..tetris::piece_size() {
-            for i in piece.pos_delta[0]..tetris::piece_size() {
-                if piece.tetro[j][i] > 0 && self.pos_inbound(i+x, j+y) {
-                    self.board[j+y][i+x] = piece.piece_type;
+        for j in 0..tetris::piece_size() {
+            for i in 0..tetris::piece_size() {
+                if piece.tetro[j][i] > 0 {
+                    self.board[ph_uod!(j,y)][ph_uod!(i,x)] = piece.piece_type;
                 }
             }
         }
     }
-    pub fn write_back_board(&mut self, x: usize, y: usize, piece: &tetris::Tetromino) {
-        for j in piece.v_iter() {
-            for i in piece.h_iter() {
-                if piece.tetro[j][i] > 0 && self.pos_inbound(i+x, j+y) {
-                    self.back_board[j+y][i+x] = piece.piece_type;
+    pub fn write_back_board(&mut self, x: i32, y: i32, piece: &tetris::Tetromino) {
+        for j in 0..tetris::piece_size() {
+            for i in 0..tetris::piece_size() {
+                if piece.tetro[j][i] > 0 {
+                    self.back_board[ph_uod!(j,y)][ph_uod!(i,x)] = piece.piece_type;
                 }
             }
         }
@@ -62,23 +86,29 @@ impl Board {
             }
         }
     }
-    pub fn collision_checker(& self, x: usize, y: usize, piece: &tetris::Tetromino) -> Blocked {
+    // FIXME fixed handling of directions
+    #[allow(unused_must_use)]
+    pub fn collision_checker(& self, x: i32, y: i32, piece: &tetris::Tetromino) -> Blocked {
         let mut pi =  *piece;
-        let mut dir = Blocked{..Default::default()};
-        for j in piece.v_iter() {
-            for i in piece.h_iter() {
-                if piece.tetro[j][i] > 0 {
-                    dir.right   = x+i >= board_size().0 - 1 || self.back_board[y+j][x+i+1] > 0 || dir.right;
-                    dir.down    = y+j >= board_size().1 - 1 || self.back_board[y+j+1][x+i] > 0 || dir.down; 
-                } 
-            }
-        }
         pi.rotate();
-        for j in pi.v_iter() {
-            for i in pi.h_iter() {
+        let mut dir = Blocked{..Default::default()};
+        
+        for j in 0..tetris::piece_size() {
+            for i in 0..tetris::piece_size() {
+                if piece.tetro[j][i] > 0 {
+                    if pos_handler(i,x-1).is_err() {dir.left == true;}
+                    else if self.back_board[ph_u!(j,y)][ph_u!(i,x-1)] > 0 {dir.left = true;}
+                    
+                    if ph_uod!(i,x+1) >= board_size().0 - 1 {dir.right == true;}
+                    else if self.back_board[ph_u!(j,y)][ph_u!(i,x+1)] > 0 {dir.right = true;}
+                    
+                    if ph_u!(j,y) >= board_size().1 - 1 {dir.down == true;}
+                    else if self.back_board[ph_u!(j,y+1)][ph_uod!(i,x)] > 0 {dir.down = true;}
+                }
                 if pi.tetro[j][i] > 0 {
-                    dir.rotate = x+i <= 0 || x+i >= board_size().0 - 1 || self.back_board[y+j][x+i] > 0 || dir.rotate;
-                } 
+                    // left || right || down || overlap
+                    if pos_handler(i,x).is_err() || ph_uod!(i,x) >= board_size().0 || ph_uod!(j,y) >= board_size().1 || self.back_board[ph_uod!(j,y)][ph_uod!(i,x)] > 0 {dir.rotate = true;}
+                }
             }
         }
         dir
